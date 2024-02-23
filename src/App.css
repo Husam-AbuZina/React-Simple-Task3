62e11fc45ddfc2f9ac2a36d52b6adfdcebcc52a0
Husam-AbuZina <husamzinap@gmail.com>
2024-02-23 22:02:13 +0300

    first commit

---
+++ src/App.css
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Counter from './components/Counter/Counter';
+
+function App() {
+ 
+  return (
+    <>
+    <Counter />
+    </>
+  )
+}
+
+export default App
